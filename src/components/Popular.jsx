a9806715e6b14a86d08a2a6d13313b5f9bd58930
Utkarsh Singh <utkarsh.singh.19@mountblue.tech>
2022-04-02 12:16:02 +0530

    added some dummy data and some npm packages

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import { useEffect, useState } from "react"
+import data from "../dummy.json"
 function Popular() {
+    const [popular, setPopular] = useState([]);
+    useEffect(() => {
+        // getPopular();
+        setPopular(data.recipes)
+    }, [])
+    // console.log(data)
+
+    // async function getPopular() {
+    //     const apiData = await (await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)).json();
+    //     // console.log(apiData)
+    //     // setPopular(apiData.recipes)
+    // }
     return (
-        <div>Popular</div>
+        <div>
+            {
+                popular.map((recipe) => {
+                    // <p>{recipe.gaps}</p>
+                    return (
+                        <div key={recipe.id}>
+                            <p >
+                                {recipe.title}
+                            </p>
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App';
 
+const container = document.getElementById('root');
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-)
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(<App />);
+// root.render(<App tab="home" />);
+
+// During an update, there's no need to pass the container again.
+// root.render(<App tab="profile" />);
